651a45a028c67dfc3545cfa71e6e53dabaaa726e
Steve Klabnik <steve@steveklabnik.com>
2011-12-05 22:06:49 -0500

    Moving top-level methods inside of Room.
    Top level methods are icky. Besides, they should really be helping the
    Room do its job, anyway. Notice how the main logic is a bit more clear
    now, with the can_go? and finished? methods.

--- maze.rb
+++ maze.rb
@@ -14,32 +14,45 @@ class Room
     @uri = uri
     @current_xml = request_xml(@uri)
   end
+
+  def can_go?(direction)
+    extract_href_from_xml(direction, @current_xml)
+  end
+
+  def finished?
+    xml_has_completed?(@current_xml)
+  end
+
+  private
+
+  def extract_href_from_xml(rel, xml)
+    doc = Nokogiri::XML(xml)
+
+    node = doc.xpath("//link[@rel=\"#{rel}\"]").first
+    return nil unless node
+    node[:href] #uuuuuuuuugh
+  end
+
+
+  def xml_has_completed?(xml)
+    Nokogiri::XML(xml).xpath("//completed").first
+  end
+
+  def request_xml(url)
+    uri = URI(url)
+
+    req = Net::HTTP::Get.new(uri.request_uri)
+    req['Accept'] = "application/xml"
+
+    res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+      http.request(req)
+    }
+
+    res.body
+  end
+
 end
 
-def request_xml(url)
-  uri = URI(url)
-
-  req = Net::HTTP::Get.new(uri.request_uri)
-  req['Accept'] = "application/xml"
-
-  res = Net::HTTP.start(uri.hostname, uri.port) {|http|
-    http.request(req)
-  }
-
-  res.body
-end
-
-def xml_has_completed?(xml)
-  Nokogiri::XML(xml).xpath("//completed").first
-end
-
-def extract_href_from_xml(rel, xml)
-  doc = Nokogiri::XML(xml)
-
-  node = doc.xpath("//link[@rel=\"#{rel}\"]").first
-  return nil unless node
-  node[:href] #uuuuuuuuugh
-end
 
 room = Room.new('http://amundsen.com/examples/mazes/2d/five-by-five/')
 
@@ -48,10 +61,10 @@ visited = {}
 path = []
 steps = 0
 
-until(xml_has_completed?(xml))
+until(room.finished?)
 
   link = ["start", "east", "west", "south", "north", "exit"].collect do |direction|
-    extract_href_from_xml(direction, xml)
+    room.can_go?(direction)
   end.find {|href| href && !visited[href] }
 
   if !link
