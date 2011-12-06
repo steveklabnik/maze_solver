6087bd61375f84bdbd0f423c72f6b50d3dc392c9
Steve Klabnik <steve@steveklabnik.com>
2011-12-05 23:02:50 -0500

    Cleanup and rename various things.
    Okay! These classes are pretty solid. Basically, this commit renames
    some methods, classes, whatever. No major changes. The most complicated
    bit is using Forwardable to make the Request class a bit nicer.

--- maze.rb
+++ maze.rb
@@ -1,30 +1,31 @@
 require 'uri'
 require 'net/http'
 require 'nokogiri'
+require 'forwardable'
 
-class Room
+class Maze
   def initialize(uri)
-    @xml ||= {}
+    @xml_cache = {}
     @uri = uri
-    @uri = xml.extract_href('start')
+    @uri = xml.href_for('start')
   end
 
-  def go(uri)
+  def visit(uri)
     @uri = uri
   end
 
   def can_go?(direction)
-    xml.extract_href(direction)
+    xml.href_for(direction)
   end
 
   def finished?
-    xml.has_completed?
+    xml.has_node?('completed')
   end
 
   private
 
   def xml
-    @xml[@uri] ||= XML.new(@uri)
+    @xml_cache[@uri] ||= XML.new(@uri)
   end
 end
 
@@ -33,33 +34,37 @@ class XML
     @uri = uri
   end
 
-  def current
-    @request ||= Request.new(@uri).get
-  end
+  def href_for(rel)
+    doc = Nokogiri::XML(self.to_s)
 
-  def extract_href(rel)
-    doc = Nokogiri::XML(current)
-
-    node = doc.xpath("//link[@rel=\"#{rel}\"]").first
+    node = doc.xpath("//link[@rel='#{rel}']").first
+    #uuuuuuuuugh, node isn't a hash, or else I'd #fetch
     return nil unless node
-    node[:href] #uuuuuuuuugh
+    node[:href]
   end
 
-  def has_completed?
-    Nokogiri::XML(current).xpath("//completed").first
+  def has_node?(node)
+    Nokogiri::XML(self.to_s).xpath("//#{node}").first
+  end
+
+  def to_s
+    @request ||= Request.new(@uri).get
   end
 end
 
 class Request
+  extend Forwardable
+  def_delegators :@uri, :request_uri, :hostname, :port
+
   def initialize(url)
     @uri = URI(url)
   end
 
   def get
-    req = Net::HTTP::Get.new(@uri.request_uri)
+    req = Net::HTTP::Get.new(request_uri)
     req['Accept'] = "application/xml"
 
-    res = Net::HTTP.start(@uri.hostname, @uri.port) {|http|
+    res = Net::HTTP.start(hostname, port) {|http|
       http.request(req)
     }
 
@@ -67,17 +72,16 @@ class Request
   end
 end
 
-
-room = Room.new('http://amundsen.com/examples/mazes/2d/five-by-five/')
+maze = Maze.new('http://amundsen.com/examples/mazes/2d/five-by-five/')
 
 visited = {}
 path = []
 steps = 0
 
-until(room.finished?)
+until(maze.finished?)
 
   link = ["start", "east", "west", "south", "north", "exit"].collect do |direction|
-    room.can_go?(direction)
+    maze.can_go?(direction)
   end.find {|href| href && !visited[href] }
 
   if !link
@@ -87,7 +91,7 @@ until(room.finished?)
 
   visited[link] = true
   path << link
-  room.go(link)
+  maze.visit(link)
 
   steps = steps + 1
 end
